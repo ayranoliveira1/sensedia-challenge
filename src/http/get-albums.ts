953f1715561900e0f930808fa60df2e8bf7f904e
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-03 23:07:35 -0300

    feat: add get albums request

---
+++ src/http/get-albums.ts
@@ -0,0 +1,30 @@
+'use server'
+
+interface Album {
+  id: string
+  title: string
+  description: string
+  user_id: string
+  updated_at: string
+  created_at: string
+}
+
+interface GetAlbumsResponse {
+  albums: Album[]
+}
+
+export const getAlbums = async (): Promise<GetAlbumsResponse> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/albums`, {
+    method: 'GET',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch albums')
+  }
+
+  const data = await res.json()
+
+  return {
+    albums: data.albums,
+  }
+}
