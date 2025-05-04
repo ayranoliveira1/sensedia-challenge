db4190c481115e572d117ce17c6619d2c7af9d33
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-03 23:07:08 -0300

    feat: add get user request

---
+++ src/http/get-users.ts
@@ -0,0 +1,29 @@
+'use server'
+
+interface User {
+  id: string
+  name: string
+  email: string
+  updated_at: string
+  created_at: string
+}
+
+interface GetUsersResponse {
+  users: User[]
+}
+
+export const getUsers = async (): Promise<GetUsersResponse> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users`, {
+    method: 'GET',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch users')
+  }
+
+  const data = await res.json()
+
+  return {
+    users: data.users,
+  }
+}
