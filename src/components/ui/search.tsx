31b3527299caa28c307a9f1e7b6b0592c68315e8
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-03 23:08:21 -0300

    feat: add search component

---
+++ src/components/ui/search.tsx
@@ -0,0 +1,29 @@
+import { IoIosSearch } from 'react-icons/io'
+
+interface SearchProps {
+  search: string
+  setSearch: (search: string) => void
+  setPage: (page: number) => void
+}
+
+const Search: React.FC<SearchProps> = ({ search, setSearch, setPage }) => {
+  return (
+    <div className="relative mb-8">
+      <input
+        type="text"
+        placeholder="Procurar"
+        className="w-full border-b bg-gray-100 py-2 pl-5 text-sm focus:outline-none"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value)
+          setPage(1)
+        }}
+      />
+      <span className="absolute right-3 top-2 text-gray-600">
+        <IoIosSearch className="size-6 " />
+      </span>
+    </div>
+  )
+}
+
+export default Search
