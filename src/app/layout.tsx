d661dab1fdfc1ccc9b51446c2961347829e1ee68
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-04 22:51:50 -0300

    feat: add sonner component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
+import { Toaster } from 'sonner'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}>{children}</body>
+      <body className={`${roboto.className} antialiased`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   )
 }
