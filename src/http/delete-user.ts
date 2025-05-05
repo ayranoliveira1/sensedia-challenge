5f622594ea7c7119ff6aa0381e14d84f432f3b56
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-04 22:54:22 -0300

    feat: add delete user request

---
+++ src/http/delete-user.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+
+export const deleteUser = async (user_id: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/${user_id}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to delete user')
+  }
+
+  revalidatePath('/')
+
+  const data = await res.json()
+
+  return data
+}
