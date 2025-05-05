403c2daf556069b7f39aaeb3e95bafe880a5b4d8
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-04 22:54:08 -0300

    feat: add create user request

---
+++ src/http/create-user.ts
@@ -0,0 +1,44 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+
+export interface CreateUsersResponse {
+  id: string
+  name: string
+}
+
+export interface CreateUserRequest {
+  name: string
+  email: string
+}
+
+export const createUser = async ({
+  name,
+  email,
+}: CreateUserRequest): Promise<CreateUsersResponse | string> => {
+  const password = process.env.PASSWORD
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/create`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, name, password }),
+    },
+  )
+
+  if (!res.ok) {
+    return res.statusText.toString()
+  }
+
+  revalidatePath('/')
+
+  const data = await res.json()
+
+  return {
+    id: data.user.id,
+    name: data.user.name,
+  }
+}
