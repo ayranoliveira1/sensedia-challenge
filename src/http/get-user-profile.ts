7f7ea79f8c6d81dad00559b21e0de67a20fd4546
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-06 13:24:33 -0300

    feat: add get user profile request

---
+++ src/http/get-user-profile.ts
@@ -0,0 +1,23 @@
+'use server'
+
+interface GetUsersResponse {
+  name: string
+  menu: {
+    label: string
+    route: string
+  }[]
+}
+
+export const getUserProfile = async (): Promise<GetUsersResponse> => {
+  const res = await fetch(`http://localhost:3000/api/user`, {
+    method: 'GET',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch user metadata')
+  }
+
+  const data = await res.json()
+
+  return data
+}
